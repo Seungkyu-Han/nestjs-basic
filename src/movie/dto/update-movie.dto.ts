a5f3dbb2f57445381f32eb1915ad5bb1e8e6a713
Seungkyu-Han <trust1204@gmail.com>
2025-11-16 12:26:05 +0900

    refactor: use class validator

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class UpdateMovieDto {
+  @IsNotEmpty()
   id: number;
+
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   genre: string;
 }
