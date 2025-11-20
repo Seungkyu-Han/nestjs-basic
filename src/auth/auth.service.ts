a1d09c42073af58cf45f30a8d1558eda86e4d108
Seungkyu-Han <trust1204@gmail.com>
2025-11-20 22:08:28 +0900

    feat: register and login api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  parseBasicToken(rawToken: string): { email: string; password: string } {
+    if (!rawToken || !rawToken.startsWith('Basic '))
+      throw new BadRequestException('Invalid authorization header');
+    const basicSplit = rawToken.split(' ');
+    if (basicSplit.length !== 2)
+      throw new BadRequestException('Invalid authorization header');
+
+    const decoded = Buffer.from(basicSplit[1], 'base64').toString('utf-8');
+    const credentialSplit = decoded.split(':');
+    if (credentialSplit.length !== 2)
+      throw new BadRequestException('Invalid authorization header');
+
+    const [email, password] = credentialSplit;
+    return { email, password };
+  }
+
+  async registerUser(rawToken: string) {
+    const { email, password } = this.parseBasicToken(rawToken);
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+    });
+
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const encrypted = await bcrypt.hash(
+      password,
+      this.configService.get<number>('HASH_ROUNDS', 10),
+    );
+
+    await this.userRepository.save({
+      email,
+      password: encrypted,
+    });
+  }
+
+  async login(rawToken: string) {
+    const { email, password } = this.parseBasicToken(rawToken);
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    const accessTokenSecret: string = this.configService.get<string>(
+      'ACCESS_TOKEN_SECRET',
+      '',
+    );
+    const refreshTokenSecret: string = this.configService.get<string>(
+      'REFRESH_TOKEN_SECRET',
+      '',
+    );
+
+    return {
+      accessToken: await this.jwtService.signAsync(
+        {
+          sub: user.id,
+          role: user.role,
+          type: 'access',
+        },
+        { secret: accessTokenSecret, expiresIn: '15m' },
+      ),
+      refreshToken: await this.jwtService.signAsync(
+        {
+          sub: user.id,
+          role: user.role,
+          type: 'refresh',
+        },
+        { secret: refreshTokenSecret, expiresIn: '45m' },
+      ),
+    };
+  }
+}
